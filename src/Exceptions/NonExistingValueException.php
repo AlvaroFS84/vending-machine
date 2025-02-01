d5a3bd28d87ac3cf3382012b10692cc5ed667b82
Álvaro <alvarofernandezsalado@gmail.com>
2025-02-01 16:44:38 +0100

    text fix

--- src/Exceptions/NonExistingValueException.php
+++ src/Exceptions/NonExistingValueException.php
@@ -8,6 +8,6 @@ class NonExistingValueException extends VendingException
 {
     public function __construct()
     {
-        parent::__construct('You inserted a no valid coin value', 400);
+        parent::__construct('You inserted a non valid coin value', 400);
     }
 }
